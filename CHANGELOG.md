f674eca72627a709ed368c026e0d5979318ca31a
Changjian Gao <gaochangjian@gmail.com>
2022-12-15 15:19:55 +0800

    Fix heading contains inline code

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog
+
+## 0.0.3
+
+- Fix heading contains inline code
+
+## 0.0.2
+
+- Add LICENSE
+
+## 0.0.1
+
+- First release
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { visit } from 'unist-util-visit';
 
 function visitor(node) {
   const data = node.data || (node.data = {});
-  const heading = node['children'][0]['value'];
+  const children = node['children'];
+  const lastChild = children[children.length - 1];  // The heading ID is always the last child
+  const heading = lastChild['value'];
   const parsedHeading = parseMarkdownHeadingId(heading);
   if (parsedHeading.id !== undefined) {
     data.id = parsedHeading.id;
